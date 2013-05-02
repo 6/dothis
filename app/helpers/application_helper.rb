3499e1447d1e09a7ce01ad30ad2a729244bbe894
Peter Graham <peterghm@gmail.com>
2013-05-01 23:38:33 -0700

    Use a default layout by default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  # http://stackoverflow.com/a/2962410/659050
+  def inside_layout(layout, &block)
+    render :inline => capture_haml(&block), :layout => "layouts/#{layout}"
+  end
+
   def link_to_active(text, path, options = {})
     if current_page?(path)
       options[:class] ||= ""
