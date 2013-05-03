5c532ba1d3fccb5b577987563366671ee20d457a
Peter Graham <peterghm@gmail.com>
2013-05-02 21:01:51 -0700

    logged in vs logged out homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Dothis::Application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   resources :users
+  resources :tasks
   resources :sessions
 
   get '/:username' => 'users#show', :as => :profile
 
-  root :to => 'users#index'
+  root :to => 'users#index', :constraints => lambda { |request| !request.cookies['auth_token'] }
+  root :to => 'tasks#index', :constraints => lambda { |request| !!request.cookies['auth_token'] }
 end
