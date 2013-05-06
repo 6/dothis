35957525f3b3133b34db184246f29744fb72a669
Peter Graham <peterghm@gmail.com>
2013-05-06 01:42:57 -0700

    add order column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130430032642) do
+ActiveRecord::Schema.define(:version => 20130506083841) do
 
   create_table "tasks", :force => true do |t|
     t.integer  "user_id",      :null => false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130430032642) do
     t.datetime "completed_at"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "order"
   end
 
   create_table "users", :force => true do |t|
