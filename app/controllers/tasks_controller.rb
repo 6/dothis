aa3668079820f3788f871870665cfca7da0250f3
Peter Graham <peterghm@gmail.com>
2013-05-06 00:47:28 -0700

    add tasks#destroy support

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,4 +16,8 @@ class TasksController < ApplicationController
   def update
     respond_with(@task = current_user.tasks.find(params[:id]).update_from_backbone_params(params))
   end
+
+  def destroy
+    respond_with(@task = current_user.tasks.find(params[:id]).destroy)
+  end
 end
