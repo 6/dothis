d2b6999c86f5cb06e791787be4be392024a72b90
Peter Graham <peterghm@gmail.com>
2013-05-09 01:15:14 -0700

    Use abbreviated month

--- vendor/assets/javascripts/cal-heatmap.js
+++ vendor/assets/javascripts/cal-heatmap.js
@@ -294,7 +294,7 @@ var CalHeatMap = function() {
       },
       format: {
         date: "%B %Y",
-        legend: "%B",
+        legend: "%b",
         connector: "on"
       },
       extractUnit : function(d) { return d.getMonth(); }
